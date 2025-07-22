65a86e2fd4aaa22421f579ba220657dc7ad1237c
Pranav Nair <pranavnairop090@gmail.com>
2025-07-22 21:42:39 +0530

    d

---
+++ personal_information_collector.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+hobby = input("Enter your hobby: ")
+
+print(f"Hello, {name}!\nYou are {age} years old and live in {city}\nIn you free time, you enjoy {hobby}.")

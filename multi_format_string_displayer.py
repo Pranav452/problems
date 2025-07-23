12f43dd58d169e8ffa332e31890372353150bdc6
Pranav Nair <pranavnairop090@gmail.com>
2025-07-23 10:46:48 +0530

    f

---
+++ multi_format_string_displayer.py
@@ -0,0 +1,12 @@
+name = "Alice"
+age = 28
+city = "Delhi"
+
+# Using % formatting
+print("My name is %s, I am %d years old, and I live in %s." % (name, age, city))
+
+# Using .format() method
+print("My name is {}, I am {} years old, and I live in {}.".format(name, age, city))
+
+# Using f-strings
+print(f"My name is {name}, I am {age} years old, and I live in {city}.")
